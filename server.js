14921a1ef88378a6ea111d6207f26726e1e44d1a
Samila-Castro <samilav1997@gmail.com>
2024-11-01 18:07:07 -0300

    feat(*2): adiciona rota de criação de eventos

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ import express from "express";
 import process  from "os";
 
 const app = express();
+app.use(express.json());
+
+const events =[];
+
+function isValidUTCDate(dateString){
+  const date = new Date(dateString);
+   if(date.toISOString() === dateString){
+    return true;
+   }
+
+  return false;
+}
 
 app.get("/health-check", (req,res)=> {
   const healthCheck = {
@@ -17,5 +29,29 @@ app.get("/health-check", (req,res)=> {
     res.status(500).send()
   }
 });
+
+app.post("/events", (req, res)=>{
+  const { name, date, location, description} = req.body;
+
+  if((!name || !date || location === "")){
+    return res.status(400).send({text: "Os campos name, date e location são obrigatórios"});
+  }
+ 
+  if(isValidUTCDate(date)){
+    const newEvent = {
+      name: name,
+      date: date,
+      location: location,
+      description: description
+    };
+
+    events.push(newEvent);
+    return res.status(200).send(newEvent);
+  }
+
+  return res.status(400).send({text: "O formato precisa ser UTC"});
+  
+});
+
 
 app.listen(3000, () => console.log("Server running on port 3000"));
